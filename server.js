ba1a8b8da127fb00891b74228dd8cdd05e71c94e
unknown <kilash@ttu.ee>
2018-12-10 02:01:34 +0200

    Add projects page

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get('/about', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    //response.send("About page");
+    response.render("projects.hbs", {
+        pageTitle: "Projects"
+    });
+});
+
 app.get('/bad', (request, response) => {
     response.send({errorMessage: "Unable to handle request!"});
 });
